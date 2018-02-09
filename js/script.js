a03b7c373011301148fbed5ad483e65c54437d3e
lipovskaia <natalia.lipovskaia@gmail.com>
2018-02-08 21:27:02 -0800

    Assignment 1
    Assignment 1 complete

---
+++ js/script.js
@@ -0,0 +1,132 @@
+// STEP 1
+/* var someMonth;
+function theMonth;
+currentMonth 
+var summerMonth;
+*/ 
+// STEP 2
+console.log("hello there");
+console.log(1001);
+console.log(null);
+console.log(false);
+// STEP 3
+var x =10, y = 5, z= 8;
+var average;
+average = (x + y + z)/3;
+console.log(average);
+// STEP 4 
+var firstName;
+var lastName; 
+var address; 
+var city;
+var state; 
+var zipCode;
+var yourAge; 
+var referralSource;
+var mayWeContactYou;
+// STEP 5
+var firstName, LastName, address; 
+firstName = "Natalia";
+lastName = "Lipovskaia";
+address = "Street 1";
+var city = "San Diego";
+var state = "CA";
+var zipCode = "92108";
+
+var yourAge = 29, referralSource = "none", mayWeContactYou = "yes";
+// STEP 6 
+var a = 30;
+window.console.log("Value is " + a + " dollars ");
+
+var b = 30<100;
+window.console.log( "30$ is less than 100$ - " + b );
+
+
+var c = 30;
+window.console.log(c > 10);
+
+// STEP 7
+
+window.console.log("My last name is "  + last + ".");
+var last = "Lipovskaia";
+// STEP 8
+var someString = "Who once said, \"Only two things are infinite, the universe and human stupidity, and I'm not sure about the former.\" ";
+window.console.log(someString);
+
+ // STEP 9
+var x = null;
+window.console.log(x);
+
+var y;
+window.console.log(y);
+// STEP 10
+window.console.log(typeof "Natalie"); //string
+window.console.log(typeof 5.15); //number
+window.console.log(typeof false); //boolean
+window.console.log(typeof [1,2,3,4]); //object
+window.console.log(typeof myName); //undefined
+// STEP 11
+window.alert("Hello " +  "Natalia " + ", welcome to the JavaScript class!");
+//STEP12
+var name;
+name = "Natalia";
+window.alert("Hello " +  name + ", welcome to the JavaScript class!");
+//STEP13
+var course;
+course = "JavaScript";
+var name;
+name = "Natalia";
+window.alert("Hello " +  name + ", welcome to the " + course + "class!");
+//Step14
+window.alert("Hello "+ name + " , \n welcome to the " + course  + " class!\n" ); 
+//Step15
+window.alert("Hello "+ prompt("Enter your name") + " , \n welcome to the " + course + " class!\n" ); 
+//STEP16
+window.alert("Hello "+ prompt("Enter your name") + " , \n welcome to the " + prompt("Enter your class") + " class!\n" ); 
+
+//STEP17
+x = 10;
+y= 20;
+var sum = x+ y;
+window.console.log(sum);
+//STEP18
+var x = 20+20;
+window.console.log(x);
+//STEP19
+var x = 20 * 5;
+window.console.log(x);
+//STEP20
+var x = 20%3 / 1;
+window.console.log(x);
+//step21
+var y = 10;
+var z = 11;
+window.console.log(z!=y);
+//STEP22
+var y = 10;
+var z = 11;
+window.console.log(z<y);
+//STEP23
+var widget = new Object();
+window.console.log(typeof widget);
+//STEP24
+var widget = new Object();
+window.console.log( widget instanceof Object );
+//STEP25
+var unWidget = "this is string";
+var widget = new Object();
+window.console.log( unWidget instanceof Object );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
